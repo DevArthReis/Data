43741d6fb935b30bbf65db7793c3caa6e841e68b
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2024-12-18 17:52:44 -0300

    Att

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ namespace Data{
             Console.WriteLine($"Ao chegar as {Data} vc sera avisado ");
             Console.WriteLine($"Qual sera o dia que vc sera avisado {Data.DayOfWeek}");
             Console.WriteLine($"Qual sera o ano que vc sera avisado {Data.DayOfYear}");
+            var Formatada = String.Format("{0}");
+            Console.WriteLine(Formatada);
             
             
             
