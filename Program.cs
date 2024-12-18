638b0c8648dbcd63e431fd364d9d9d16b3d958ad
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2024-12-18 17:48:53 -0300

    Data

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Data{
+    class Program{
+        static void Main(string[] args){
+            Console.Clear();
+            //var Data = new DateTime(2024,12,18);
+            var Data = DateTime.Now;
+            Console.WriteLine(Data.Day);
+            Console.WriteLine(Data.Year);
+            Console.WriteLine(Data.Month);
+            Console.WriteLine(Data.Hour);
+            Console.WriteLine(Data.Minute);
+            Console.WriteLine(Data.Second);
+            Console.WriteLine(Data);
+            Console.WriteLine($"Ao chegar as {Data} vc sera avisado ");
+            Console.WriteLine($"Qual sera o dia que vc sera avisado {Data.DayOfWeek}");
+            Console.WriteLine($"Qual sera o ano que vc sera avisado {Data.DayOfYear}");
+            
+            
+            
+            
+        }
+    }
+}
